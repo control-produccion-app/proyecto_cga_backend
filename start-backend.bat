39285027772df6a72f5680e0e217ee35038e9d78
Fabrizio Javier Bugedo Gonzalez <fa.bugedo@duocuc.cl>
2026-04-16 23:34:27 -0400

    Delete start-backend.bat
    Limpieza

--- start-backend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Script batch para iniciar el backend Django
-REM Usa PowerShell con politica bypass para ejecutar el script .ps1
-
-echo Iniciando backend Django...
-powershell -ExecutionPolicy Bypass -File "%~dp0start-backend.ps1"
-pause
